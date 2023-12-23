2c655e96d75d97d1cd30ea0b94f9a55f627ce2f6
Florent Guiliani <florent@guiliani.fr>
2023-12-23 20:02:54 +0000

    day 9 part 2

---
+++ day9/part2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import re
+
+# Open and read the file
+with open('day9/input.txt', 'r') as file:
+    lines = file.readlines()
+
+#print(lines)
+    
+measures = [[list(map(int, re.findall(r'-?\d+', l)))] for l in lines]
+
+for i in range(len(measures)):
+    while True:
+        seq = measures[i][-1]
+        print(seq)
+        diffs = []
+        for j in range(1, len(seq)):
+            diffs += [seq[j] - seq[j - 1]]
+        measures[i] += [diffs]
+        if len(diffs) == 1:
+            break
+        if sum(1 for x in diffs if x == 0) == len(diffs):
+            break
+    #break
+
+score = 0
+print(len(measures))
+
+#print(measures[0])
+for m in measures:
+    #print(m)
+    prev = 0
+    while m:
+        last = m.pop()
+        print(last)
+        prev = last[0] - prev
+        print("Prev:", prev)
+    print("Score:", prev)
+    score += prev
+    #break
+
+print("Final score:", score)
